ff95535923ad9bd83385d54daaf19f3462ea7c50
rdong8 <66289396+rdong8@users.noreply.github.com>
2023-01-18 22:41:55 -0500

    Added basic workstation config

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 function setup() {
-  sudo dnf install ansible -y
+  sudo dnf install ansible snapd -y
+  sudo ln -s /var/lib/snapd/snap /snap
 
   sudo touch /var/log/workstation/ansible.log
   echo -e  "\n# Start workstation-cfg/setup.sh" >> ~/.bashrc
